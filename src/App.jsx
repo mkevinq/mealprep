601bfd5392673a2b8f7a4ddd1150b51ab585d222
AnirudhTiku <62532928+AnirudhTiku@users.noreply.github.com>
2020-11-28 16:58:05 -0500

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -4,17 +4,14 @@ import {
   Route,
   Link
 } from "react-router-dom";
-import Card from './Card.jsx';
-import iphone from "./assets/homepage/iphone-preview1.png";
-import iphones2 from "./assets/homepage/iphones-preview-2.png";
-import butter_chicken from "./assets/order-page/recipes-images/butter-chicken.png";
-import lime_chicken_burritos from "./assets/order-page/recipes-images/lime-chicken-burrito.png";
+import placeholder from "./assets/placeholder_phone.png";
+import placeholder2 from "./assets/placeholder_phones.png";
 import logo from './assets/navbar/full-logo.svg';
 import cart from './assets/navbar/cart.svg';
 import account from './assets/navbar/account.svg';
-import food0 from './assets/order-page/small-dish.png';
-import food1 from './assets/order-page/dish-1.png';
-import food2 from './assets/order-page/dish-2.png';
+import magnify from './assets/homepage/magnify-icon.svg';
+import plan_steps from './assets/homepage/plan-icon.svg';
+import delivery from './assets/homepage/delivery-icon.svg';
 
 function App() {
   return (
@@ -23,91 +20,7 @@ function App() {
         { /* Search page */}
         <Route path="/search">
           <NavBar />
-
-          <div className="search">
-            <h1 id="search_title">
-              Browse Recipes
-            </h1>
-            <input id="search_bar" placeholder="What are you looking for?">
-            </input>
-            <img class="floating-food" id="browse-food-0" src={food0} alt=""></img>
-            <img class="floating-food" id="browse-food-1" src={food1} alt=""></img>
-            <img class="floating-food" id="browse-food-2" src={food2} alt=""></img>
-          </div>
-
-          { /* Header */}
-          <div className = "search-headers">
-            <h1 id="title">
-              <b>Search Your Favourites</b>
-            </h1>
-            <hr align="left"></hr>
-            <p id="description">
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut laboreLorem ipsum dolor sit amet, consectetur.
-            </p>
-          </div>
-
-          <div className='flex'>
-            <Card title='Butter Chicken' rating={4.9} time={45} calories={450} servings={6} imgsrc={butter_chicken}/> 
-            <Card title='Lime Chicken Burritos' rating={4.8} time={30} calories={300} servings={8} imgsrc={lime_chicken_burritos}/> 
-            <Card title='Lime Chicken Burritos' rating={4.9} time={45} calories={450} servings={6} imgsrc={butter_chicken}/> 
-          </div>
-
-          { /* Header */}
-          <div className = "search-headers">
-            <h1 id="title">
-              <b>On the menu this week</b>
-            </h1>
-            <hr align="left"></hr>
-          </div>
-
-          <div className= "continer">
-            {/* Column 1 */}
-            <div className ="col1">
-              <h3>
-                Create Healthy Meal Plan
-              </h3>
-              <hr className ="line"></hr>
-              <p>
-                Lorm ipsum dolor amet, consectetur adipiscing elit, <br></br>
-                sed do eiusmod tempor inciddunt ut labore
-              </p>
-            </div>
-            {/* Column 2 */}
-            <div className ="col2">
-              <button className="browse">Browse</button>
-            </div>
-          </div>
-
-          <div className="container2">
-            <div className ="icon-col">
-              <img id="logo" src={logo} alt="Logo"></img>
-            </div>
-            <div className ="text-col">
-              <div className ="About-col">
-                <Link to="/" style={{textDecoration:"none", color:"#131A2C"}}>
-                  <p>
-                    <strong>
-                      About
-                    </strong>
-                  </p>
-                </Link>
-              </div>
-              <div className="Plan-col">
-                <p>
-                  <strong>
-                    Plan
-                  </strong>
-                </p>
-              </div>
-              <div className="Order-col">
-                <p>
-                  <strong>
-                    Order
-                  </strong>
-                </p>
-              </div>
-            </div>
-          </div>
+          <Link to="/">Home</Link>
         </Route>
 
         { /* Test page */}
@@ -139,6 +52,37 @@ function App() {
               Learn More
             </button>
           </div>
+s            <img id = "magnifying_glass" src = {magnify} alt = "magnifying glass" width = "36.48" height = "36.48"/>
+            <div id = "two_million">
+              Over 2,000,000+
+            </div>
+            <div id = "browse">
+              Browse Recipies
+            </div>
+            <div id = "lorem_ipsum">
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore
+            </div>
+            <img id = "steps" src = {plan_steps} alt = "plan" width = "48" height = "48"/>
+            <div id = "health">
+            Healthy & affordable
+            </div>
+            <div id = "create">
+              Create Plan
+            </div>
+            <div id = "lorem_ipsum2">
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore
+            </div>
+            <img id = "truck" src = {delivery} alt = "delivery truck" width = "30" height = "28.8"/>
+            <div id = "local">
+              Locally Sourced
+            </div>
+            <div id = "groceries">
+              Groceries Delivered
+            </div>
+            <div id = "lorem_ipsum3">
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore
+            </div>
+         
 
           <div class="highlight" id="highlight1">
             <div id="text_left">
@@ -158,11 +102,11 @@ function App() {
                 </button>
               </Link>
             </div>
-            <img id="img_right" src={iphone} alt="Phone" width="214" height="430"/>
+            <img id="img_right" src={placeholder} alt="Phone" width="214" height="430"/>
           </div>
 
           <div class="highlight" id="highlight2">
-            <img id="img_left" src={iphones2} alt="Two Phones" width="455" height="428"/>
+            <img id="img_left" src={placeholder2} alt="Two Phones" width="331" height="428"/>
             <div id="text_right">
               <h6 id="subtitle">
                 SECTION 2 SUB TITLE
@@ -196,21 +140,23 @@ function App() {
             <div className ="col2">
               <button className="browse">Browse</button>
             </div>
-          </div>
+        </div>
 
           <div className="container2">
             <div className ="icon-col">
-              <img id="logo" src={logo} alt="Logo"></img>
+              <h6>
+                <strong>
+                  mealprep.
+                </strong>
+              </h6>
             </div>
             <div className ="text-col">
               <div className ="About-col">
-                <Link to="/" style={{textDecoration:"none", color:"#131A2C"}}>
-                  <p>
-                    <strong>
-                      About
-                    </strong>
-                  </p>
-                </Link>
+                <p>
+                  <strong>
+                    About
+                  </strong>
+                </p>
               </div>
               <div className="Plan-col">
                 <p>
@@ -242,9 +188,7 @@ function NavBar() {
       <img id="logo" src={logo} alt="Logo"></img>
       <div class="right flex-center">
         <div class="links flex">
-          <Link to="/" style={{textDecoration:"none", color:"#131A2C"}}>
-            <p>ABOUT</p>
-          </Link>
+          <p>ABOUT</p>
           <Link to="/search" style={{textDecoration:"none", color:"#131A2C"}}>
             <p>PLAN</p>
           </Link>
